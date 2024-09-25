5003178d4801b743d2a1569f495ddf9480f23a38
Muhammad Faris Ammar <amirul.ahj@gmail.com>
2024-09-25 14:43:09 +0800

    adding form to upload file for avatar

---
+++ resources/views/profile/partials/user-avatar-form.blade.php
@@ -0,0 +1,36 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('User Avatar') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __("Update your account's profile information and email address.") }}
+        </p>
+    </header>
+    {{------------------------------------------------------------------- 
+        <form method="post" action="route('profile.avatar')">    
+        this is the traditional way to do it
+        
+        remember to do Form Method Spoofing
+    -------------------------------------------------------------------}}
+    <form method="post" action="{{ route('profile.avatar') }}">
+        {{-------------------------------------------------------------------
+            <input type="hidden" name="_method" value="patch"> 
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+            this is another way to do it
+        -------------------------------------------------------------------}}
+        @method('patch')
+        @csrf
+
+        <div>
+            <x-input-label for="name" :value="__('Avatar')" />
+            <x-text-input id="avatar" name="avatar" type="file" class="mt-1 block w-full" :value="old('avatar', $user->avatar)" required autofocus autocomplete="avatar" />
+            <x-input-error class="mt-2" :messages="$errors->get('avatar')" />
+        </div>
+        <div class="flex items-center gap-4 mt-4">
+            <x-primary-button>{{ __('Save') }}</x-primary-button>
+        </div>
+    </form>
+</section>
